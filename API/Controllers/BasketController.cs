132be7baef61365528b8dabb746cb9d394e8afcc
Denys Leon <leonden0046@gmail.com>
2024-02-07 21:07:05 +0200

    Added controller endpoints for CustomerBasket

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,39 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BasketController : BaseApiController
+{
+    private readonly IBasketRepository _basketRepository;
+    
+    public BasketController(IBasketRepository basketRepository)
+    {
+        _basketRepository = basketRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+    {
+        var basket = await _basketRepository.GetBasketAsync(id);
+
+        return Ok(basket ?? new CustomerBasket(id));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket customerBasket)
+    {
+        var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
+
+        return Ok(updatedBasket);
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteBasketAsync(string id)
+    {
+        await _basketRepository.DeleteBasketAsync(id);
+
+        return NoContent();
+    }
+}
